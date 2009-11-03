14a049906314d62224a5c9798e728a96447c1d24
Ryan Gee <rgee@wesleyan.edu>
2009-11-03 16:09:59 +0000

    Renamed Skydome to OldSkyDome in preparation for a new skydome component.

---
+++ Sky/Sky/OldSkyDome.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Sky
+{
+    /// <summary>
+    /// This class represents a "sky" made up of
+    /// six textures. (A TextureCube)
+    /// </summary>
+    class OldSkyDome
+    {
+        GraphicsDevice device;
+        Model skyBoxModel;
+        TextureCube skyBoxTexture;
+
+        public OldSkyDome()
+        {
+        }
+
+        public void LoadContent(GraphicsDevice device, Model skyBoxModel, TextureCube skyBoxTexture)
+        {
+            this.device = device;
+            this.skyBoxModel = skyBoxModel;
+            this.skyBoxTexture = skyBoxTexture;
+        }
+
+        public void Draw(FirstPersonCamera cam)
+        {
+            device.RenderState.DepthBufferWriteEnable = false;
+            foreach (ModelMesh mesh in skyBoxModel.Meshes)
+            {
+                foreach (Effect effect in mesh.Effects)
+                {
+                    effect.Parameters["Texture0"].SetValue(skyBoxTexture);
+                    effect.Parameters["matView"].SetValue(cam.View);
+                    effect.Parameters["matProj"].SetValue(cam.Projection);
+                }
+                mesh.Draw();
+            }
+            device.RenderState.DepthBufferWriteEnable = true;
+
+            // Temporarily set to false in shader.
+            device.RenderState.DepthBufferEnable = true;
+        }
+    }
+}
